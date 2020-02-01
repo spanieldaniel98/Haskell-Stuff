1f8c6453c68ebc459bf24b649d180c7571b4cf77
Daniel Queiros <damq1g17@soton.ac.uk>
2020-02-01 04:08:42 +0000

    removed output .txt files, added telekeypad converter

--- Utilities/TeleKeypadConverter.hs
+++ Utilities/TeleKeypadConverter.hs
@@ -34,10 +34,10 @@ toTeleKeypad = do
     else do 
       blank
       putStrLn $ id $ toDigits $ removeExtraSpaces input
-      writeFile "output.txt" $ id $ toDigits $ removeExtraSpaces input
+      writeFile "teleKeypadOutput.txt" $ id $ toDigits $ removeExtraSpaces input
       blank
       toTeleKeypad 
-                     
+
 fromTeleKeypad :: IO ()
 fromTeleKeypad = do 
     putStrLn "Enter line to convert from telephone keypad digits (press Enter with no input to return to menu): "
@@ -47,21 +47,22 @@ fromTeleKeypad = do
     else do 
       blank
       putStrLn $ id $ fromDigits $ removeExtraSpaces input
-      writeFile "output.txt" $ id $ fromDigits $ removeExtraSpaces input
+      writeFile "teleKeypadOutput.txt" $ id $ fromDigits $ removeExtraSpaces input
       blank
       fromTeleKeypad 
-                       
-removeExtraSpaces :: String -> String
-removeExtraSpaces "" = ""
-removeExtraSpaces (' ' : ' ' : rest) = removeExtraSpaces (' ' : rest)
-removeExtraSpaces (x:rest) = x : removeExtraSpaces rest
 
--- Converts an English string to telephone keypad digits                   
+-- remove extra spaces from string (so no two consecutive spaces exist)
+removeExtraSpaces :: String -> String
+removeExtraSpaces ""                 = ""
+removeExtraSpaces (' ' : ' ' : rest) = removeExtraSpaces (' ' : rest)
+removeExtraSpaces (x:rest)           = x : removeExtraSpaces rest
+
+-- converts an English string to telephone keypad digits                   
 toDigits :: String -> String
 toDigits "" = ""
 toDigits [x] = toDigits' x
 toDigits (x : ' ' : rest) = toDigits' x ++ " " ++ toDigits rest
-toDigits (x : rest)   = toDigits' x ++ "-" ++ toDigits rest
+toDigits (x : rest)       = toDigits' x ++ "-" ++ toDigits rest
 
 toDigits' :: Char -> String
 toDigits' 'a' = "2"
@@ -82,7 +83,7 @@ toDigits' 'o' = "666"
 toDigits' 'p' = "7"
 toDigits' 'q' = "77"
 toDigits' 'r' = "777"
-toDigits' 's'= "7777"
+toDigits' 's' = "7777"
 toDigits' 't' = "8"
 toDigits' 'u' = "88"
 toDigits' 'v' = "888"
@@ -95,38 +96,38 @@ toDigits' '_' = "0"
 toDigits' '#' = "↑"
 toDigits' _   = "!"
 
--- Converts a string of telephone keypad digits to English
+-- convert a string of telephone keypad digits to English
 fromDigits :: String -> String
 fromDigits "" = ""
-fromDigits (' ' : rest) = " " ++ fromDigits rest
-fromDigits ('-' : rest)   = fromDigits rest
-fromDigits ('2' : '2' : '2' : rest) = "c" ++ fromDigits rest
-fromDigits ('2' : '2' : rest) = "b" ++ fromDigits rest
-fromDigits ('2' : rest) = "a" ++ fromDigits rest
-fromDigits ('3' : '3' : '3' : rest) = "f" ++ fromDigits rest
-fromDigits ('3' : '3' : rest) = "e" ++ fromDigits rest
-fromDigits ('3' : rest) = "d" ++ fromDigits rest
-fromDigits ('4' : '4' : '4' : rest) = "i" ++ fromDigits rest
-fromDigits ('4' : '4' : rest) = "h" ++ fromDigits rest
-fromDigits ('4' : rest) = "g" ++ fromDigits rest
-fromDigits ('5' : '5' : '5' : rest) = "l" ++ fromDigits rest
-fromDigits ('5' : '5' : rest) = "k" ++ fromDigits rest
-fromDigits ('5' : rest) = "j" ++ fromDigits rest
-fromDigits ('6' : '6' : '6' : rest) = "o" ++ fromDigits rest
-fromDigits ('6' : '6' : rest) = "n" ++ fromDigits rest
-fromDigits ('6' : rest) = "m" ++ fromDigits rest
+fromDigits ('-' : rest)                   = fromDigits rest
+fromDigits (' ' : rest)                   = " " ++ fromDigits rest
+fromDigits ('2' : '2' : '2' : rest)       = "c" ++ fromDigits rest
+fromDigits ('2' : '2' : rest)             = "b" ++ fromDigits rest
+fromDigits ('2' : rest)                   = "a" ++ fromDigits rest
+fromDigits ('3' : '3' : '3' : rest)       = "f" ++ fromDigits rest
+fromDigits ('3' : '3' : rest)             = "e" ++ fromDigits rest
+fromDigits ('3' : rest)                   = "d" ++ fromDigits rest
+fromDigits ('4' : '4' : '4' : rest)       = "i" ++ fromDigits rest
+fromDigits ('4' : '4' : rest)             = "h" ++ fromDigits rest
+fromDigits ('4' : rest)                   = "g" ++ fromDigits rest
+fromDigits ('5' : '5' : '5' : rest)       = "l" ++ fromDigits rest
+fromDigits ('5' : '5' : rest)             = "k" ++ fromDigits rest
+fromDigits ('5' : rest)                   = "j" ++ fromDigits rest
+fromDigits ('6' : '6' : '6' : rest)       = "o" ++ fromDigits rest
+fromDigits ('6' : '6' : rest)             = "n" ++ fromDigits rest
+fromDigits ('6' : rest)                   = "m" ++ fromDigits rest
 fromDigits ('7' : '7' : '7' : '7' : rest) = "s" ++ fromDigits rest
-fromDigits ('7' : '7' : '7' : rest) = "r" ++ fromDigits rest
-fromDigits ('7' : '7' : rest) = "q" ++ fromDigits rest
-fromDigits ('7' : rest) = "p" ++ fromDigits rest
-fromDigits ('8' : '8' : '8' : rest) = "v" ++ fromDigits rest
-fromDigits ('8' : '8' : rest) = "u" ++ fromDigits rest
-fromDigits ('8' : rest) = "t" ++ fromDigits rest
+fromDigits ('7' : '7' : '7' : rest)       = "r" ++ fromDigits rest
+fromDigits ('7' : '7' : rest)             = "q" ++ fromDigits rest
+fromDigits ('7' : rest)                   = "p" ++ fromDigits rest
+fromDigits ('8' : '8' : '8' : rest)       = "v" ++ fromDigits rest
+fromDigits ('8' : '8' : rest)             = "u" ++ fromDigits rest
+fromDigits ('8' : rest)                   = "t" ++ fromDigits rest
 fromDigits ('9' : '9' : '9' : '9' : rest) = "z" ++ fromDigits rest
-fromDigits ('9' : '9' : '9' : rest) = "y" ++ fromDigits rest
-fromDigits ('9' : '9' : rest) = "x" ++ fromDigits rest
-fromDigits ('9' : rest) = "w" ++ fromDigits rest
-fromDigits ('*' : rest) = "+" ++ fromDigits rest
-fromDigits ('0' : rest) = "_" ++ fromDigits rest
-fromDigits ('↑' : rest) = "#" ++ fromDigits rest
-fromDigits (_ : rest) = "!" ++ fromDigits rest
+fromDigits ('9' : '9' : '9' : rest)       = "y" ++ fromDigits rest
+fromDigits ('9' : '9' : rest)             = "x" ++ fromDigits rest
+fromDigits ('9' : rest)                   = "w" ++ fromDigits rest
+fromDigits ('*' : rest)                   = "+" ++ fromDigits rest
+fromDigits ('0' : rest)                   = "_" ++ fromDigits rest
+fromDigits ('↑' : rest)                   = "#" ++ fromDigits rest
+fromDigits (_ : rest)                     = "!" ++ fromDigits rest
